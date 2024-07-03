89fe9096e48879e66674f953e2d0ad7752a9ffda
LuuBaro <baolgpc08011@fpt.edu.vn>
2024-07-03 22:03:41 +0700

    Bao entity

---
+++ src/com/inevntory/entity/Products.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inevntory.entity;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Products {
+
+    private String ProductName;
+    private String Type;
+    private int Quantity;
+    private Double price;
+
+    public Products(String ProductName, String Type, int Quantity, Double price) {
+        this.ProductName = ProductName;
+        this.Type = Type;
+        this.Quantity = Quantity;
+        this.price = price;
+    }
+
+    public Products() {
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String ProductName) {
+        this.ProductName = ProductName;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int Quantity) {
+        this.Quantity = Quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+}
